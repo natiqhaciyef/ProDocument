56267ab064229698380e96c937a1d2120193e41a
natiqhaciyef <natiq00h2272@gmail.com>
2024-02-07 22:54:17 +0400

    Upgrade: Half part of date picker and CompleteProfileAccount screen created

--- app/src/main/java/com/natiqhaciyef/prodocument/ui/view/registration/create_account/CompleteProfileFragment.kt
+++ app/src/main/java/com/natiqhaciyef/prodocument/ui/view/registration/create_account/CompleteProfileFragment.kt
@@ -1,14 +1,19 @@
 package com.natiqhaciyef.prodocument.ui.view.registration.create_account
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.natiqhaciyef.prodocument.R
+import com.natiqhaciyef.prodocument.databinding.AlertDialogCalendarViewBinding
 import com.natiqhaciyef.prodocument.databinding.FragmentCompleteProfileBinding
 import com.natiqhaciyef.prodocument.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 
 @AndroidEntryPoint
@@ -25,6 +30,34 @@ class CompleteProfileFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val calendar = Calendar.getInstance()
+        with(binding) {
+            datePickerDialog(calendar)
+        }
+    }
 
+    private fun datePickerDialog(calendar: Calendar) {
+        val datePicker = DatePickerDialog.OnDateSetListener { datePicker, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            changeCalendar(calendar)
+        }
+
+        binding.completeProfileDOBInput.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                datePicker,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+            ).show()
+        }
+    }
+
+    private fun changeCalendar(calendar: Calendar) {
+        val format = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(format, Locale.UK)
+        println(sdf.format(calendar.time))
     }
 }
