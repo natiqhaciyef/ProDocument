410167635b469ba440aca44ef47a1912b4c3802b
natiqhaciyef <natiq00h2272@gmail.com>
2024-03-31 22:41:19 +0400

    Config: Base Mock generator created

--- data/src/main/java/com/natiqhaciyef/data/network/NetworkResult.kt
+++ data/src/main/java/com/natiqhaciyef/data/network/NetworkResult.kt
@@ -3,13 +3,14 @@ package com.natiqhaciyef.data.network
 import retrofit2.HttpException
 import retrofit2.Response
 
-sealed class NetworkResult<T : Any> {
+sealed class NetworkResult<T> {
     class Success<T: Any>(val data: T) : NetworkResult<T>()
     class Error<T: Any>(val code: Int, val message: String?) : NetworkResult<T>()
     class Exception<T: Any>(val e: Throwable) : NetworkResult<T>()
 }
 
 suspend fun <T : Any> handleNetworkResponse(
+    // get mock class
     execute: suspend () -> Response<T>
 ): NetworkResult<T> {
     return try {
