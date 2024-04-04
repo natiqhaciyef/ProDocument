7c31e41b2df1376166bbd475fef2e6df62e59088
natighajiyev <natig.hajiyev1@gmail.com>
2024-04-04 09:38:07 +0400

    Upgrade: User id removed

--- app/src/main/java/com/natiqhaciyef/prodocument/ui/util/DefaultImplModels.kt
+++ app/src/main/java/com/natiqhaciyef/prodocument/ui/util/DefaultImplModels.kt
@@ -6,7 +6,6 @@ import com.natiqhaciyef.common.model.mapped.MappedUserModel
 
 object DefaultImplModels {
     val mappedUserModel = MappedUserModel(
-        id = "0",
         name = "",
         password = "",
         email = "",
