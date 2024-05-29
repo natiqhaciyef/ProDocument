d6824859d4becf91c22f8711b3c246d31cf91d57
natighajiyev <natig.hajiyev1@gmail.com>
2024-05-29 16:10:29 +0400

    Upgrade: Subscription Adapter config update

--- app/src/main/java/com/natiqhaciyef/prodocument/ui/view/main/premium/PremiumFragment.kt
+++ app/src/main/java/com/natiqhaciyef/prodocument/ui/view/main/premium/PremiumFragment.kt
@@ -1,6 +1,7 @@
 package com.natiqhaciyef.prodocument.ui.view.main.premium
 
 import android.os.Bundle
+import android.telephony.SubscriptionPlan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import com.natiqhaciyef.prodocument.databinding.FragmentPremiumBinding
 import com.natiqhaciyef.core.base.ui.BaseFragment
 import com.natiqhaciyef.prodocument.ui.view.main.MainActivity
 import com.natiqhaciyef.prodocument.ui.view.main.home.options.scan.adapter.ScanViewPagerAdapter
+import com.natiqhaciyef.prodocument.ui.view.main.premium.adapter.SubscriptionPlanAdapter
 import com.natiqhaciyef.prodocument.ui.view.main.premium.contract.PremiumContract
 import com.natiqhaciyef.prodocument.ui.view.main.premium.viewmodel.PremiumViewModel
 import com.natiqhaciyef.prodocument.ui.view.onboarding.behaviour.ZoomOutPageTransformer
@@ -82,7 +84,7 @@ class PremiumFragment(
         for (subscriptionModel in list){
             plansFragmentList.add(SubscriptionFragment(subscriptionModel = subscriptionModel))
         }
-        val adapter = ScanViewPagerAdapter(plansFragmentList, this@PremiumFragment)
+        val adapter = SubscriptionPlanAdapter(plansFragmentList, this@PremiumFragment)
         binding.subscriptionViewPager.adapter = adapter
         binding.subscriptionViewPager.setPageTransformer(ZoomOutPageTransformer())
     }
